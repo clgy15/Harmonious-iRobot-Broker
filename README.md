30930dcb917e1947ac1b3ed32a712bc708809485
Brian Whitlow <clgy15@gmail.com>
2014-04-19 14:42:05 -0400

    Basic Functionality and Synch Complete
    Mutli-robot Communication proven. Note input completed and error
    handling and connection closing have been finished.

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-Harmonious-iRobot-Broker
-========================
+#Harmonious iRobot Broker
+
+Node.js server broker designed to control tempo and to coordinate all robots. It's primary responsibility is a tempo server and the same time also takes care of communicating the state of each iRobot to all other iRobots.
+
+
+##Communication Protocol
+
+####Broker Server TCP packets for each Tick:
+
+	Byte 0: 1 => Loop, 2 => Beat, 0 => Neither??
+	Byte 1: # of Beats in the loop. Always include
+	Byte 2: # of Robots playing. Always include
+	Byte 3 - # of Robots + 3: Each Playing iRobot's note on that beat.
+
+####iRobot Initialization Protocol:
+When a iRobot creates a new connection to the Broker Server, it is added to the Clients array of open connections and it will receive the same TCP packets above that the Playing iRobot's receive. However, it will not be identified as a Playing Robot until it returns the following Packet:
+
+	Byte 0: 0x01 <= Current Initialization ID
+	Byte 1 - # of Beats: The note that it will play for each Beat in order
+
+
+####Special Case #1: 0 Robot's are playing
+This is our initialization case. When the 1st robot comes on-line, the Broker server must send it the notes it will play instead of the notes the other robot's are playing for obvious reasons. User input currently has simple frequency insertion.
+
+	Byte 0: 1 => Loop, 2 => Beat
+	Byte 1: # of Beats in the loop.
+	Byte 2: 0 Playing Robots <= Identifies special case for iRobot
+	Byte 3: Broker Entered note assigned to first Robot
+
+
+####Bb Scale
+
+70
+70
+72
+74
+75
+77
+79
+81
+82
+82
+81
+79
+77
+75
+74
+72
+70
+70
--- index.js
+++ index.js
@@ -1,101 +1,168 @@
 // Load the TCP Library
 net = require('net');
 
+//Brian's to do:
+//   1. Fix error handling
+
 // Keep track of the chat clients
 var clients = [];
+var init_notes = [];
 
 var beats_per_loop = 0;
 var current_beat = 1;
 var num_robots = 0;
+var milli_time = 500;
 
 var prompt = require('prompt');
 
 var schema = {
-    properties: {
-      beats: {
-        pattern: /^[0-9]+$/,
-        message: 'Number of beats must be a number',
-        required: true
-      }
+  properties: {
+    beats: {
+      pattern: /^[0-9]+$/,
+      message: 'Number of beats must be a number',
+      required: true
     }
-  };
+  }
+};
+
+
+
+var client_server = net.createServer(function (socket) {
+
+  // Identify this client
+  socket.name = socket.remoteAddress + ":" + socket.remotePort;
+  socket.init = 0;
+  socket.notes = []
+  clients.push(socket);
+
+  // Send a nice welcome message and announce
+  console.log("Received connection from: " + socket.remoteAddress);
+
+  socket.on('data', function(data) {
+    console.log("Recieved Something: " + data);
+
+    if (data.readUInt8(0) == 1) {
+      console.log("iRobot coming online...");
+      for (var i = 1; i <= beats_per_loop; i++) {
+        socket.notes.push(data.readUInt8(i));
+      }
+      console.log(socket.notes)
+      socket.init = 2;
+      num_robots++;
+    }
+    else if (data.readUInt8(0) == 0) {
+      console.log("Client Stopped Playing: .... Good Day Robot!");
+
+      socket.init = 0;
+      num_robots--;
+    }
+
+  });
+
+  socket.on('end', function() {
+    console.log("Client closed connection: .... Good Day Robot!");
+
+    loc = clients.indexOf(socket);
+    clients.splice(loc,1);
+    num_robots--;
+  });
+
+  //Currenty, any error on client side will remove the client from our array list
+  socket.on('error', function(err){
+      // Handle the connection error.
+      console.log("Hit an error: " + err + ".... Current Protocol is to remove iRobot. Good Day Robot!");
+
+      loc = clients.indexOf(socket);
+      clients.splice(loc,1);
+      num_robots--;
+  });
+
+
+});
+
+
+///BEGINNING OF PROGRAM
+
+
 
 prompt.start();
 
 prompt.get(schema, function (err,result) {
   beats_per_loop = result.beats;
 
-  // Start a TCP Server
-  net.createServer(function (socket) {
-
-    // Identify this client
-    socket.name = socket.remoteAddress + ":" + socket.remotePort;
-    socket.note = '\x44';
-    socket.init = 0;
-    clients.push(socket);
-
-    // Send a nice welcome message and announce
-    console.log("Received connection from: " + socket.remoteAddress);
-
-    //Byte 0: 1 => Loop, 2 => Beat, 0 => Neither??
-    //Byte 1: # of Beats in the loop. Always include
-    //Byte 2: # of Robots playing
-    //Byte 3 - # of Robots + 3: Sequence of bytes representing what each robot is playing
-    var temp = '\x02\x04\x01\x44\x53\x66\x44\r\n';
-    console.log(temp.length);
-
-  }).listen(4454, '127.0.0.1');
-
-  setInterval(function(){
-    var string_parse = ''
-
-    //Beat or Loop
-    if (current_beat == beats_per_loop) {
-      console.log('Loop');
-      current_beat = 0;
-
-      //Check for new clients!
-      clients.forEach(function(client) {
-        //If the new connection hasn't seen a Loop yet...
-        if (client.init == 0) {
-          client.init = 1;
-        }
-        //If the new connection now has listened to a complete loop....
-        //Increment number of robots..
-        //Set iRobot to fully initialized...
-        else if (client.init == 1) {
-          client.init = 2;
-          num_robots++;
-        }
-      });
-
-      string_parse = '\x01'
-
+  var beat_schema = {
+    properties: {
+      notes: {
+        pattern: /^[0-9]+$/,
+        message: 'Enter beats: ',
+        required: true,
+        type: 'array',
+        maxItems: beats_per_loop,
+        minItems: beats_per_loop
+      }
     }
-    else {
-      console.log('Tick');
-      string_parse = '\x02'
-    }
+  };
 
-    //Now add # of Beats & # of Playing Robots
-    string_parse += String.fromCharCode(beats_per_loop) + String.fromCharCode(num_robots);
-    clients.forEach(function (client) {
+  prompt.get(beat_schema, function (err,result) {
+    init_notes = result.notes;
+    console.log(init_notes);
 
-      //If init == 2, then client is fully initialized and playing
-      if (client.init == 2) {
-        string_parse += client.note;
+    client_server.listen(4454, '192.168.1.2');
+
+    setInterval(function(){
+      var string_parse = ''
+
+      //Beat or Loop
+      if (current_beat == beats_per_loop) {
+        //console.log('Loop');
+        current_beat = 0;
+
+        //Check for new clients!
+        clients.forEach(function(client) {
+          //If the new connection hasn't seen a Loop yet...
+          if (client.init == 0) {
+            client.init = 1;
+          }
+
+        });
+
+        string_parse = '\x01'
+
+      }
+      else {
+        //console.log('Tick');
+        string_parse = '\x02'
       }
 
-    });
+      //Now add # of Beats & # of Playing Robots
+      string_parse += String.fromCharCode(beats_per_loop) + String.fromCharCode(num_robots)+String.fromCharCode(milli_time/1000*64);
+      if (num_robots == 0) {
+        string_parse += String.fromCharCode(init_notes[current_beat]);
+      }
+      else {
+        clients.forEach(function (client) {
+          //If init == 2, then client is fully initialized and playing
+          if (client.init == 2) {
+            string_parse += String.fromCharCode(client.notes[current_beat]);
+          }
+        });
+      }
 
-    string_parse += '\r\n';
+      string_parse += '\r\n';
 
-    clients.forEach(function (client) {
-      client.write(string_parse);
-    });
+      var temp = []
+      for (var i = 0; i < string_parse.length; i++) {
+        temp.push(string_parse.charCodeAt(i));
+      }
+      console.log(temp);
+      clients.forEach(function (client) {
+        client.write(string_parse);
+      });
 
-    current_beat++;
-    console.log(clients.length + " Clients Connected & " + num_robots + " Robot's Playing");
+      current_beat++;
+      //console.log(clients.length + " Clients Connected & " + num_robots + " Robot's Playing");
 
-  }, 500);
+    }, milli_time);
+  });
+
 });
