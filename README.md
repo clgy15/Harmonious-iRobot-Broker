e5334953481b13ff5290ed67fe8711ef728ed51d
Brian Whitlow <clgy15@gmail.com>
2014-04-20 17:27:39 -0400

    Beat + Length included
    Length values now associated and processed.

--- README.md
+++ README.md
@@ -7,17 +7,30 @@ Node.js server broker designed to control tempo and to coordinate all robots. It
 
 ####Broker Server TCP packets for each Tick:
 
-	Byte 0: 1 => Loop, 2 => Beat, 0 => Neither??
+	Byte 0: 1 => Loop, 2 => Beat
 	Byte 1: # of Beats in the loop. Always include
 	Byte 2: # of Robots playing. Always include
-	Byte 3 - # of Robots + 3: Each Playing iRobot's note on that beat.
+	Byte 3: Smallest unit of note length in multiples of 1/64th of a second.
+	Byte 4.... n - 2 : Each Playing iRobot's note on that beat, Length of beat currently being played
+	End Bytes: '/r/n' to signify end of sequence
 
+	Example:
+	[1, 1, 1, 32, 55, 1, 13, 10] -- Loop, 1 Beat in Loop, 1 Robot Online, 32/64 seconds, Note 55, 1 length in beat, /r, /n
 ####iRobot Initialization Protocol:
 When a iRobot creates a new connection to the Broker Server, it is added to the Clients array of open connections and it will receive the same TCP packets above that the Playing iRobot's receive. However, it will not be identified as a Playing Robot until it returns the following Packet:
 
 	Byte 0: 0x01 <= Current Initialization ID
-	Byte 1 - # of Beats: The note that it will play for each Beat in order
+	Byte 1, 2 - # of Beats: The note that it will play for each Beat in order, Length of note
 
+####iRobot Deinitialization Protocol:
+When an iRobot **stops playing** (AKA: the Pause button is pressed), the following will be send:
+
+	Byte 0: 0x00 <= Stop Playing ID
+
+Any extra bytes are *Don't Cares*.
+This will cause the Playing robot's counter to decrement, however the iRobot is not removed from the connections list
+
+When an iRobot **goes offline or shuts down**, the iRobot will either send an 'end' signal or the server will encounter a *TIMEOUT*, or *ERR*. In all three cases, the default action by the server will be to decrement the Playing robot's counter and then to remove the iRobot from the connections list.
 
 ####Special Case #1: 0 Robot's are playing
 This is our initialization case. When the 1st robot comes on-line, the Broker server must send it the notes it will play instead of the notes the other robot's are playing for obvious reasons. User input currently has simple frequency insertion.
@@ -25,26 +38,50 @@ This is our initialization case. When the 1st robot comes on-line, the Broker se
 	Byte 0: 1 => Loop, 2 => Beat
 	Byte 1: # of Beats in the loop.
 	Byte 2: 0 Playing Robots <= Identifies special case for iRobot
-	Byte 3: Broker Entered note assigned to first Robot
+	Byte 3: Smallest unit of note length in multiples of 1/64th of a second.
+	Byte 4: Broker Entered note assigned to first Robot
+	Byte 5: Length of note currently playing.
 
+
+##Compiled Songs
 
 ####Bb Scale
 
-70
-70
-72
-74
-75
-77
-79
-81
-82
-82
-81
-79
-77
-75
-74
-72
-70
-70
+	14
+	70, 2
+	72, 1
+	74, 1
+	75, 1
+	77, 1
+	79, 1
+	81, 1
+	82, 2
+	81, 1
+	79, 1
+	77, 1
+	75, 1
+	74, 1
+	72, 1
+
+
+####Happy Lyric
+	19
+	0,1
+	48, 1
+	48, 2
+	51, 1
+	60, 1
+	60, 1
+	65, 1
+	65, 1
+	65, 3
+	65, 2
+	65, 2
+	65, 3
+	65, 1
+	63, 1
+	65, 2
+	65, 2
+	65, 2
+	63, 1
+	65, 4
--- index.js
+++ index.js
@@ -2,12 +2,15 @@
 net = require('net');
 
 //Brian's to do:
-//   1. Fix error handling
+//   1. Each Robot now has a note length
+//   ex. 49 - length 2 49 - length 2 0 - length 1 51 - length 1
 
 // Keep track of the chat clients
 var clients = [];
 var init_notes = [];
+var init_lengths = [];
 
+var notes_per_loop = 0;
 var beats_per_loop = 0;
 var current_beat = 1;
 var num_robots = 0;
@@ -19,7 +22,7 @@ var schema = {
   properties: {
     beats: {
       pattern: /^[0-9]+$/,
-      message: 'Number of beats must be a number',
+      message: 'Number of notes must be a number',
       required: true
     }
   }
@@ -33,6 +36,7 @@ var client_server = net.createServer(function (socket) {
   socket.name = socket.remoteAddress + ":" + socket.remotePort;
   socket.init = 0;
   socket.notes = []
+  socket.lengths = []
   clients.push(socket);
 
   // Send a nice welcome message and announce
@@ -43,8 +47,9 @@ var client_server = net.createServer(function (socket) {
 
     if (data.readUInt8(0) == 1) {
       console.log("iRobot coming online...");
-      for (var i = 1; i <= beats_per_loop; i++) {
+      for (var i = 1; i <= beats_per_loop; i+=2) {
         socket.notes.push(data.readUInt8(i));
+        socket.lengths.push(data.readUInt8(++i));
       }
       console.log(socket.notes)
       socket.init = 2;
@@ -88,24 +93,34 @@ var client_server = net.createServer(function (socket) {
 prompt.start();
 
 prompt.get(schema, function (err,result) {
-  beats_per_loop = result.beats;
+  notes_per_loop = result.beats;
 
   var beat_schema = {
     properties: {
       notes: {
         pattern: /^[0-9]+$/,
-        message: 'Enter beats: ',
+        message: 'Enter beats & length (Note, Length)): ',
         required: true,
         type: 'array',
-        maxItems: beats_per_loop,
-        minItems: beats_per_loop
-      }
+        maxItems: notes_per_loop,
+        minItems: notes_per_loop
+      },
     }
   };
 
   prompt.get(beat_schema, function (err,result) {
-    init_notes = result.notes;
+    result.notes.forEach(function(note) {
+      temp_arr = note.split(',');
+      for (var i = 0; i < +temp_arr[1]; i++) {
+        init_notes.push(temp_arr[0].trim());
+        init_lengths.push(temp_arr[1].trim());
+        beats_per_loop++;
+      }
+    });
+    //init_notes = result.notes.split();
     console.log(init_notes);
+    console.log(init_lengths);
+    console.log(beats_per_loop);
 
     client_server.listen(4454, '192.168.1.2');
 
@@ -138,12 +153,14 @@ prompt.get(schema, function (err,result) {
       string_parse += String.fromCharCode(beats_per_loop) + String.fromCharCode(num_robots)+String.fromCharCode(milli_time/1000*64);
       if (num_robots == 0) {
         string_parse += String.fromCharCode(init_notes[current_beat]);
+        string_parse += String.fromCharCode(init_lengths[current_beat]);
       }
       else {
         clients.forEach(function (client) {
           //If init == 2, then client is fully initialized and playing
           if (client.init == 2) {
             string_parse += String.fromCharCode(client.notes[current_beat]);
+            string_parse += String.fromCharCode(client.lengths[current_beat]);
           }
         });
       }
