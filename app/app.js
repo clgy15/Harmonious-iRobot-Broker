dc918fd0ee2c28b219423b09c02a3dcb66d44430
Jeff Principe <princjef@gmail.com>
2014-04-23 10:32:30 -0400

    added pattern definition and user preference forms

--- app/app.js
+++ app/app.js
@@ -17,6 +17,40 @@ angular.module('goodVibrations').service('socket', [function() {
   };
 }]);
 
+angular.module('goodVibrations').directive('noteSelect', [function() {
+  return {
+    restrict: 'E',
+    scope: {
+      noteOptions: '=',
+      note: '=ngModel'
+    },
+    controller: function($scope) {
+      $scope.currentIndex = 0;
+      $scope.note = $scope.noteOptions[$scope.currentIndex];
+
+      $scope.nextNoteOption = function() {
+        if ($scope.currentIndex >= $scope.noteOptions.length - 1) {
+          $scope.currentIndex = 0;
+        } else {
+          $scope.currentIndex++;
+        }
+        $scope.note = $scope.noteOptions[$scope.currentIndex];
+      };
+
+      $scope.prevNoteOption = function() {
+        if ($scope.currentIndex <= 0) {
+          $scope.currentIndex = $scope.noteOptions.length - 1;
+        } else {
+          $scope.currentIndex--;
+        }
+        $scope.note = $scope.noteOptions[$scope.currentIndex];
+      };
+    },
+    replace: true,
+    template: '<div class="note-select"><div class="left-toggle" ng-click="prevNoteOption()">&lt;</div><div class="note-display"><img width="50px" height="100px" ng-src="{{note.path}}" /></div><div class="right-toggle" ng-click="nextNoteOption()">&gt;</div></div>'
+  };
+}]);
+
 angular.module('goodVibrations').controller('PageCtrl', ['$scope', 'socket', function($scope, socket) {
   socket.on('connect', function(data) {
     console.log("got", data);
@@ -28,7 +62,91 @@ angular.module('goodVibrations').controller('UserParamsCtrl', ['$scope', 'socket
   $scope.tempo = 120;
   $scope.maxLoops = 4;
   $scope.notes = [];
-  $scope.durations = [];
+
+  $scope.noteType = $scope.notes[0];
+  $scope.pitch = 68;
+
+  $scope.noteTypes = [
+    {
+      name: "Quarter Note",
+      duration: 2,
+      path: "/icons/quarter_note.svg",
+      isRest: false
+    }, {
+      name: "Eighth Note",
+      duration: 1,
+      path: "/icons/eighth_note.svg",
+      isRest: false
+    }, {
+      name: "Half Note",
+      duration: 4,
+      path: "/icons/half_note.svg",
+      isRest: false
+    }, {
+      name: "Rest",
+      duration: 1,
+      path: "/icons/eighth_rest.svg",
+      isRest: true
+    }
+  ];
+
+  $scope.addNote = function(type, pitch) {
+    if (type.duration + $scope.patternLength() <= $scope.beatCount) {
+      if (type.isRest) {
+        $scope.notes.push({
+          duration: type.duration,
+          pitch: 0
+        });
+      } else {
+        $scope.notes.push({
+          duration: type.duration,
+          pitch: pitch
+        });
+      }
+    } else {
+      console.log("Max pattern length exceeded");
+    }
+  };
+
+  $scope.getNoteImage = function(duration, pitch) {
+    if (pitch === 0) {
+      switch (duration) {
+      case 1:
+        return "/icons/eighth_rest.svg";
+        break;
+      }
+    } else {
+      switch (duration) {
+      case 1:
+        return "/icons/eighth_note.svg";
+        break;
+      case 2:
+        return "/icons/quarter_note.svg";
+        break;
+      case 4:
+        return "/icons/half_note.svg";
+      }
+    }
+  };
+
+  $scope.patternLength = function() {
+    var totalLength = 0;
+    $scope.notes.forEach(function(note) {
+      totalLength += note.duration;
+    });
+    return totalLength;
+  };
+
+  $scope.getNoteName = function(pitch) {
+    if (pitch === 0) {
+      return "N/A"
+    } else {
+      var note = "";
+      var octave = "";
+      var notes = ["C", "C#", "D", "Eb", "E", "F", "F#", "G", "Ab", "A", "Bb", "B"];
+      return notes[pitch % 12] + (pitch / 12 >> 0);
+    }
+  };
 
   $scope.started = false;
 
@@ -38,8 +156,7 @@ angular.module('goodVibrations').controller('UserParamsCtrl', ['$scope', 'socket
         beatCount: $scope.beatCount,
         duration: ((64 * 60) / $scope.tempo) >> 0,
         maxLoops: $scope.maxLoops,
-        notes: $scope.notes,
-        durations: $scope.durations
+        notes: $scope.notes
       });
       $scope.started = true;
     }
